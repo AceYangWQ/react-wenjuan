5c5a4f233cf7d2744893d43b4f6211c15801899f
yangwuqi <1120817870@qq.com>
2023-10-21 14:55:45 +0800

    chore: finish form rules

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,103 @@
-import React, { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { FC, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Space, Typography, Form, Input, Button, Checkbox } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+
+import styles from './Login.module.scss'
+import { REGISTER_PATHNAME } from '../router'
+
+const { Title } = Typography
+
+const USERNAME_KEY = 'USERNAME'
+const PASSWORD_KEY = 'PASSWORD'
+
+const rememberInfo = (username: string, password: string) => {
+  localStorage.setItem(USERNAME_KEY, username)
+  localStorage.setItem(PASSWORD_KEY, password)
+}
+
+const deleteFromStorage = () => {
+  localStorage.removeItem(USERNAME_KEY)
+  localStorage.removeItem(PASSWORD_KEY)
+}
+
+const getFromStorage = () => {
+  const username = localStorage.getItem(USERNAME_KEY)
+  const password = localStorage.getItem(PASSWORD_KEY)
+  return {
+    username,
+    password,
+  }
+}
 
 const Login: FC = () => {
-  const nav = useNavigate()
+  const [form] = Form.useForm()
+  useEffect(() => {
+    const { username, password } = getFromStorage()
+    form.setFieldsValue({ username, password })
+  }, [form])
+  const onFinish = (values: any) => {
+    const { username, password, remember } = values || {}
+    if (remember) {
+      rememberInfo(username, password)
+    } else {
+      deleteFromStorage()
+    }
+  }
+
   return (
-    <div>
-      <p>Login</p>
+    <div className={styles.container}>
       <div>
-        <button onClick={() => nav(-1)}>返回</button>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>注册新用户</Title>
+        </Space>
+      </div>
+
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          onFinish={onFinish}
+          initialValues={{ remember: true }}
+          form={form}
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入用户名' },
+              { type: 'string', min: 5, max: 20, message: '字符长度在 5-20 之间' },
+              { pattern: /^\w+$/, message: '只能是字母数字下划线' },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[
+              { required: true, message: '请输入密码' },
+              { type: 'string', min: 6, max: 20, message: '字符长度在 6-20 之间' },
+              { pattern: /^\w+$/, message: '只能是字母数字下划线' },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 16 }}>
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to={REGISTER_PATHNAME}>去注册</Link>
+            </Space>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   )
