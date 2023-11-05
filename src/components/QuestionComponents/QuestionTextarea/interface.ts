22506cbff90f7abea6bbf454b79c15ed9ad2469c
yangwuqi <1120817870@qq.com>
2023-11-05 10:41:08 +0800

    chore: finish QuestionTextarea

--- src/components/QuestionComponents/QuestionTextarea/interface.ts
+++ src/components/QuestionComponents/QuestionTextarea/interface.ts
@@ -8,6 +8,6 @@ export type QuestionTextareaPropsType = {
 }
 
 export const QuestionTextareaDefaultProps: QuestionTextareaPropsType = {
-  title: '多行输入标题',
+  title: '输入框标题',
   placeholder: '请输入...',
 }
